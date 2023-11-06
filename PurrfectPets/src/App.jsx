a8528ee70d9bb37c0f72127dd98f2b24b8494d4c
Tu <tun42092@temple.edu>
2023-11-06 14:07:19 -0500

    added Logo For petFinder

--- PurrfectPets/src/App.jsx
+++ PurrfectPets/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/PetFinderLogo.png'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,21 +12,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.petfinder.com/" target="_blank">
+          <img src= {logo} className="logo" alt="PetFinder logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Find your Purrfect Pet</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
