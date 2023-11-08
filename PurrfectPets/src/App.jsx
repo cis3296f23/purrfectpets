146dede896eec54c2b5e5ca82fa6aab00de11ecb
Tu <tun42092@temple.edu>
2023-11-08 09:12:24 -0500

    changed bottom text box

--- PurrfectPets/src/App.jsx
+++ PurrfectPets/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import logo from './assets/PetFinderLogo.png'
 
@@ -19,11 +17,11 @@ function App() {
       <h1>Find your Purrfect Pet</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Pets Matched : {count}
         </button>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on PetFinder to learn more
       </p>
     </>
   )
