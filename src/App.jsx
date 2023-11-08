3f09c2f68c02b1fe5511ffe5fe2ac4596eeaebdf
Tu <tun42092@temple.edu>
2023-11-08 09:12:24 -0500

    changed bottom text box

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import './App.css'
 import logo from './assets/PetFinderLogo.png'
 import checkmark from './assets/checkmark.png'
 import xmark from './assets/xmark.png'
-
-
+import NavBar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [pets, setPets] = useState([]);
   const [currentPetIndex, setCurrentPetIndex] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
@@ -51,43 +49,45 @@ function App() {
 
   return (
     <>
-    <div>
-      <a href="https://www.petfinder.com/" target="_blank">
-        <img src={logo} className="logo" alt="PetFinder logo" />
-      </a>
-    </div>
-    <h1>Find your Purrfect Pet</h1>
-    <div className="card">
-      <button onClick={nextPet}>
-        Show Next Pet
-      </button>
-    </div>
-    <ul style={{ listStyle: 'none' }}>
-      {pets[currentPetIndex] && (
-        <li key={pets[currentPetIndex].id} className="pet-details">
-          <div className="pet-image-container">
-            <img
-              src={pets[currentPetIndex].photos[0]?.medium}
-              alt={pets[currentPetIndex].name}
-              onError={(e) => {
-                e.target.style.display = 'none'; // Hide the image on error
-              }}
-            />
-          </div>
-          <div className="pet-info">
-            <p><strong>Name: {pets[currentPetIndex].name}</strong></p>
-            <p><strong>Type: {pets[currentPetIndex].type}</strong></p>
-            <p><strong>Age: {pets[currentPetIndex].age}</strong></p>
-            <a href={pets[currentPetIndex].url} target="_blank" rel="noopener noreferrer">
-              Learn More
-            </a>
-          </div>
-        </li>
-      )}
-    </ul>
-    <div className="button-options">
-      <img src={xmark} width="300" onClick={handleDislike} alt="Dislike"></img>
-      <img src={checkmark} width="300" onClick={handleLike} alt="Like"></img>
+    <div className="app-container">
+      <div>
+        <a href="https://www.petfinder.com/" target="_blank">
+          <img src={logo} className="logo" alt="PetFinder logo" />
+        </a>
+      </div>
+      <h1>Find your Purrfect Pet</h1>
+      <ul style={{ listStyle: 'none' }}>
+        {pets[currentPetIndex] && (
+          <li key={pets[currentPetIndex].id} className="pet-details">
+            <div className="pet-image-container">
+              <img
+                src={pets[currentPetIndex].photos[0]?.medium}
+                alt={pets[currentPetIndex].name}
+                onError={(e) => {
+                  e.target.style.display = 'none'; // Hide the image on error
+                }}
+              />
+            </div>
+            <div className="pet-info">
+              <p><strong>Name: {pets[currentPetIndex].name}</strong></p>
+              <p><strong>Type: {pets[currentPetIndex].type}</strong></p>
+              <p><strong>Age: {pets[currentPetIndex].age}</strong></p>
+              <a href={pets[currentPetIndex].url} target="_blank" rel="noopener noreferrer">
+                Learn More
+              </a>
+            </div>
+          </li>
+        )}
+      </ul>
+      <div className="button-options">
+        <img src={xmark} width="300" onClick={handleDislike} alt="Dislike"></img>
+        <img src={checkmark} width="300" onClick={handleLike} alt="Like"></img>
+      </div>
+      <p className="read-the-docs">
+      </p>
+      <footer>
+        <a href="https://www.flaticon.com/free-icons/cats" title="cats icons">Cats icons created by Freepik - Flaticon</a>
+      </footer>
     </div>
   </>
 );
