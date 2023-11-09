85e1b830d46f5a47f1beb83f41a872451c07e84b
Tu <tun42092@temple.edu>
2023-11-08 19:43:44 -0500

    merged Dev into TU-Dev

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      <div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={Petfinder.getPets}>
