64e0b7ce911c51facc08b04655262b6ed3c327bc
Tu <tun42092@temple.edu>
2023-11-15 12:09:17 -0500

    move css elements around

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
-//import './index.css'
+import App from './App.jsx'
+import './index.css'
 import LoginSignup from './components/LoginComponents/LoginSignup.jsx'
 import Account from './components/AccountComponents/Account.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App/>
-    <LoginSignup/> */}
+    <App/>
+    <LoginSignup/>
     <Account/>
   </React.StrictMode>,
 )
