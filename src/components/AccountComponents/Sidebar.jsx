c81712f173489d385dbace5e92aebc590dc447d6
Tu <tun42092@temple.edu>
2023-11-15 10:53:55 -0500

    added a sidebar

---
+++ src/components/AccountComponents/Sidebar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import './Account.scss'
+import ProfilePic from '../../assets/ProfilePic.png'
+
+
+
+function SideBar(){
+
+
+    const sideBarItems = [
+        {
+            title: "Account",
+            icon: <img src={ProfilePic} alt="" />,
+            link: "/Account"
+
+        },
+        {
+            title: "Settings",
+            icon: <img src={ProfilePic} alt="" />,
+            link: '/Settings'
+
+        },
+        {
+            title: "Help",
+            icon: <img src={ProfilePic} alt="" />,
+            link:'/Help'
+
+        },
+        {
+            title: "Logout",
+            icon: <img src={ProfilePic} alt="" />,
+            link: '/LoginSignup'
+
+        }
+        
+
+    ]
+
+    return(
+        <div className="sideBar">
+
+            <ul className="list-group">
+                {sideBarItems.map((val,key)=>{
+                    return(
+                        <li key = {key} 
+                        className="list-item"
+                        id = {window.location.pathname == val.link? "active": ""}
+                        onClick={() => {
+                            window.location.pathname = val.link
+                        }}>
+
+                            <div id="icon">{val.icon}</div>
+                            <div id = 'title'>{val.title}</div>
+                            
+                            </li>
+                    )
+                })}
+            </ul>
+            
+        </div>
+    )
+}
+
+export default SideBar
