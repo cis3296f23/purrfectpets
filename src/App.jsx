28652856f7f405e9fbe4f85a6e87d74c17e95b19
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2023-11-16 14:45:48 -0500

    another test commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     Petfinder.getAccessToken().then(() => {
       Petfinder.getPets().then(pets => {
         setPets(pets);
-        console.log(pets);
       });
     });
   }, []);
@@ -20,22 +19,27 @@ function App() {
     <>
       <div>
         <a href="https://www.petfinder.com/" target="_blank">
-          <img src={logo} className="logo" alt="PetFinder logo" />
+          <img src= {logo} className="logo" alt="PetFinder logo" />
         </a>
       </div>
       <h1>Find your Purrfect Pet</h1>
+      <div>
       <div className="card">
+        <button onClick={Petfinder.getPets}>
+          Adopt!
+        </button>
       </div>
+      <li>
         <ul style={{listStyle: 'none'}}>
           {pets.filter(pet => pet.photos[0]?.medium).map(pet => (
             <li key={pet.id} className="pet-details">
               <div className="pet-image-container">
-                <img
-                  src={pet.photos[0]?.medium}
-                  alt={pet.name}
-                  onError={(e) => {
-                    e.target.style.display = 'none'; // Hide the image on error
-                  }} />
+                <img 
+                src={pet.photos[0]?.medium} 
+                alt={pet.name} 
+                onError={(e) => {
+              e.target.style.display = 'none'; // Hide the image on error
+          }}/>
               </div>
               <div className="pet-info">
                 <p><strong>Name: {pet.name}</strong></p>
@@ -46,12 +50,9 @@ function App() {
             </li>
           ))}
         </ul>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <footer>
-        <a href="https://www.flaticon.com/free-icons/cats" title="cats icons">Cats icons created by Freepik - Flaticon</a>
-      </footer>
+      </li>
+      </div>
+      <NavBar />
     </>
   )
 }
