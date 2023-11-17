f97443d7ab54b486861f2eea7483e98bb237da9c
Theron <theron.halsey@gmail.com>
2023-11-17 15:25:00 -0500

    added backend

---
+++ utils/controller.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import Petfinder from'./Petfinder.js'
+
+const router = express.Router();
+
+router.get("/Petfinder/GetPets/:page", async (req, res) => {
+    try {
+        const pets = await Petfinder.getPets(req.params.page);
+        res.send(pets)
+    } catch (err) {
+        res.status(500).send("Petfinder API error");
+    }
+});
+
+export default router;
