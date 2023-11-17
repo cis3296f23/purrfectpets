32731a7bfe620c470fed83e8e417f5aadaf886a1
Tu <tun42092@temple.edu>
2023-11-17 10:20:23 -0500

    fixed path issue

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return <nav className=" navBar">
 
             <button className="account-nav" onClick={()=>{
-                window.location.pathname = '/account'
+                window.location.pathname = '/Account'
 
             }}>Account</button>
             <button className="login-nav" onClick={()=>{
