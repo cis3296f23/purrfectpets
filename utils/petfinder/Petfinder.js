67dd59a73d4e12338ab075e4963cf65f7ad331c6
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2023-11-29 14:08:48 -0500

    added comments to my changed logic in Petfinder.js

--- utils/petfinder/Petfinder.js
+++ utils/petfinder/Petfinder.js
@@ -3,7 +3,7 @@ import { getUserAnimalPrefsFromInt, makeUserPrefsURL } from "../encodeDecodeUser
 dotenv.config();
 
 let accessToken = '';
-let userPrefs = 0;
+let userPrefs = -1;
 let userAnimals = [];
 let prefsURL = '';
 
@@ -22,14 +22,21 @@ async function getAccessToken() {
     setTimeout(() => accessToken = '', expiresIn);
 }
 export async function getPets(page = 1, prefs = 255) {
+    // if we do not have an access token, get one
     if (!accessToken) {
         await getAccessToken()
     }
-    if (!userPrefs || (userPrefs != prefs)) {
+    // assign prefs default value of 255 if it is out of valid range
+    if (prefs < 1 || prefs >= 8192) {
+        prefs = 255;
+    }
+    // if first time checking prefs, or if user as changed their preferences since last call to petfinder, set variables to user prefs
+    if (userPrefs != prefs) {
         userPrefs = prefs;
         userAnimals = getUserAnimalPrefsFromInt(userPrefs);
         prefsURL = makeUserPrefsURL(userPrefs);
     }
+    // fetch a random type from the user's preferences conforming to the users other preferences
     return fetch(`https://api.petfinder.com/v2/animals?type=${userAnimals[Math.floor(Math.random() * userAnimals.length)]}&page=${page}${prefsURL}`, {
         headers: {
             'Authorization': `Bearer ${accessToken}`,
