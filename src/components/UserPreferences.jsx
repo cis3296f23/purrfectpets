5ad61273b96eed57f83595156b2ec06d545d3f61
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2023-12-01 00:06:08 +0800

    Updated User Preferences list to be a convenient check-list of pets

--- src/components/UserPreferences.jsx
+++ src/components/UserPreferences.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Petfinder from'../Utils/Petfinder'
 import {Component} from "react"
 import './UserPreferences.css'
+import logo from '../assets/PetFinderLogo.png'
 
-
-function UserPreferences() {
+/*
     const [pets, setPets] = useState([]);
 
   useEffect(() => {
@@ -15,29 +15,112 @@ function UserPreferences() {
       });
     });
   }, []);
-    
+  */
 
-    return <up className=" userPreferences">
-            <>
-                <Dropdown>
-                <Element
-                name = "Dogs">
-                    <Preferences
-                    name = "Big"
-                    />
-                </Element>
-                <Element
-                    name = "Cat"/>
-                <Element
-                    name = "Fish"/>
-                </Dropdown>
-            </>
-        </up>
+  function UserPreferences() {
 
+    const [open, setOpen] = useState(false);
+    const[display, setDisplay] = useState('none')
 
+    const checkList = ["Dog", "Cat", "Rabbit", "Small & Furry", "Horse", "Bird", "Scales, Fins & Other", 
+"Barnyard", "Good with Children", "Good with Dogs", "Good with Cats", "House Trained", "Special Needs"];
+    const [checked, setChecked] = useState([]);
 
+    // Add/Remove checked item from list
+    const handleCheck = (event) => {
+        var updatedList = [...checked];
+        if (event.target.checked) {
+          updatedList = [...checked, event.target.value];
+        } else {
+          updatedList.splice(checked.indexOf(event.target.value), 1);
+        }
+        setChecked(updatedList);
+    };
+
+    // Return classes based on whether item is checked
+    const isChecked = (item) =>
+        checked.includes(item) ? "checked-item" : "not-checked-item";
+
+    // Generate string of checked items
+    var checkedItems = checked.length
+        ? checked.reduce((total, item) => {
+            return total + ", " + item;
+        })
+        : "";
+
+    function handleClick(){
+
+        if (display == 'none'){
+            setDisplay('block')
+        }else{
+            setDisplay('none')
+        }
+
+    }
+  
+    let menuRef = useRef();
+  
+    useEffect(() => {
+      let handler = (e)=>{
+        if(!menuRef.current.contains(e.target)){
+          setOpen(false);
+          console.log(menuRef.current);
+        }      
+      };
+  
+      document.addEventListener("mousedown", handler);
+      
+  
+      return() =>{
+        document.removeEventListener("mousedown", handler);
+      }
+  
+    });
+  
+    return (
+      <div className="UserPreferences">
+        <div className='Menu' ref={menuRef}>
+          <div className='Dropdown' onClick={handleClick}>
+            <h3> User Preferences </h3>
+          </div>
+  
+          <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} style = {{display:display}}>
+            <ul>
+                {checkList.map((item, index) => (
+                    <div key={index}>
+                        <input value={item} type="checkbox" onChange={handleCheck}/>
+                        <span className={isChecked(item)}>{item}</span>
+
+                    </div>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+function Element(props){
+/*
+    const[display, setDisplay] = useState('none')
+    function handleClick(){
+        if (display == 'none'){
+            setDisplay('none')
+        }else{
+            setDisplay('block')
+        }
+
+    }
+*/
+    var content = props.name
+    return(
+        <li>
+            <a>{content}</a>
+        </li>
+    )
 }
 
+/*
 function Dropdown(props){
     const[display, setDisplay] = useState('none')
     function handleClick(){
@@ -59,58 +142,9 @@ function Dropdown(props){
     )
 
 }
+*/
 
-function Element(props){
-    const[display, setDisplay] = useState('none')
-    function handleClick(){
-        if (display == 'none'){
-            setDisplay('none')
-        }else{
-            setDisplay('block')
-        }
-
-    }
-    var content = props.name
-    return(
-        <div onClick={handleClick}>
-            {content}
-            <div style = {{display:display}}>
-                {props.children}
-            </div>
-
-        </div>
-    )
-}
-
-function Preferences(props){
-    var content = props.name
-    return(
-        <div>
-            {content}
-        </div>
-    )
-
-}
-
-function DropDownButton (props){
-    var content = props.content;
-    var handleClick = props.onClick;
-    return(
-        <div>
-            <div>
-                {content}
-            </div>
-        </div>
-    )
-}
-
-
-
-export {DropDownButton}
 export {Element}
-export{Preferences}
-
-export {Dropdown}
 
 
 export default UserPreferences;
