392e861e87b46f36b18937bb816247f257bd5cae
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2023-12-02 13:22:12 -0500

    fixed a bug with login. Another bug remains but is not blocking other progress

--- src/components/LoginComponents/LoginSignup.jsx
+++ src/components/LoginComponents/LoginSignup.jsx
@@ -250,7 +250,7 @@ function LoginSignup() {
                                 setEmailAvailabilityText(checkEmailAvailability)
                                 console.log(checkUsernameAvailability)
                                 setUsernameAvailabilityText(checkUsernameAvailability)
-                                if (checkEmailAvailability && checkUsernameAvailability) {
+                                if (!checkEmailAvailability && !checkUsernameAvailability) {
                                     setEmailAvailabilityText(true)
                                     setUsernameAvailabilityText(true)
                                     if (arePasswordsEqual()) {
