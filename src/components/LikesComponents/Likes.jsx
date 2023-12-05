2f6d1e4f4f58560ce29dcf5c9d357e6f3314e5ac
Bryan Reiter <tul13138@temple.edu>
2023-12-05 15:07:02 -0500

    basic like page setup

--- src/components/LikesComponents/Likes.jsx
+++ src/components/LikesComponents/Likes.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import SideBar from "../AccountComponents/Sidebar.jsx"
+import './Likes.css'
 
 function Likes(){
     const [likes, setLikes] = useState([])
@@ -11,23 +13,31 @@ function Likes(){
             console.log("response: ", response); // log the response
             const responseBody = await response.text(); // get the response body as text
             console.log('responseBody: ', responseBody);
-            const likesData = await response.json();
+            const likesData = responseBody
             console.log("likesData: ", likesData); // log the likes data
-            const likesArray = likesData.split(',');
+            const likesArray = likesData.split(',').map(like => like.trim().replace(/"/g, ''));; // likesData is a string, split by commas
             console.log("likesArray: ", likesArray)
+            console.log("Index 0: ", likesArray[0])
             setLikes(likesArray)
         };
         fetchLikes();
     }, []);
 
     return (
-        <div>
-            <h1>Likes</h1>
-            {likes.map((like, index) => (
-                <div key={index}>
-                    <p>{like}</p>
+        <div className="likes-container">
+            <SideBar />
+            <div className="likes">
+                <div className="user-likes">
+                    <h1>Your Likes</h1>
+                    <div className="likes-display">
+                    {likes.map((like, index) => (
+                        <div key={index}>
+                            <p>{like}</p>
+                        </div>
+                    ))}
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
     );
 }
